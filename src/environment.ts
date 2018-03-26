2461d47c8dd8ebd583b4be3b9cb0ef50dc3d0299
Niklas Holtmeyer <niklas.holtmeyer@hs-osnabrueck.de>
2018-03-26 20:14:53 +0200

    init

--- src/environment.ts
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig  = {
-  apiKey: "AAA",
-  authDomain: "BBB.firebaseapp.com",
-  databaseURL: "https://CCC.firebaseio.com",
-  projectId: "DDDD",
-  storageBucket: "EEEE.appspot.com",
-  messagingSenderId: "FFF"
-};
-export const webClientIdGooglePlusApi = 'XXXX.apps.googleusercontent.com';
